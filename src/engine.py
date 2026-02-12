8416832f3a1e0a977e7bb53a64a7e2b512df74da
LuckyyyStrike <ingoschultz0@googlemail.com>
2026-02-12 22:19:55 +0100

    Fixed map file not being found

--- src/engine.py
+++ src/engine.py
@@ -11,12 +11,13 @@ class Engine:
         self.tick_interval_in_seconds = tick_interval_in_seconds
 
         print("Looking for maps.")
-        folder = "../maps"
+        folder = os.path.join(os.path.dirname(__file__), "..", "maps")
+        folder = os.path.abspath(folder)  # optional, resolves to absolute path
         files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
         file = random.choice(files)
         print("Found map file '{}'".format(file))
         lines: list[str] = []
-        with open(file, "r") as f:
+        with open(os.path.join(folder, file), "r") as f:
             lines = list(f)
         max_width = 80
         max_height = 30
